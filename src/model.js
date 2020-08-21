eb2bce8d8856cb156ac305dc11eb69cea4182f27
Kotfil <Dramen333>
2020-08-21 09:43:46 +0300

    commit new text Func

---
+++ src/model.js
@@ -0,0 +1,9 @@
+export const model = [
+    { type: 'title', value: 'Test Title', options: {}},
+    {type: 'text',value: 'Lorem 2222222222222', options: {}},
+    {type: 'textColumns',value: [
+        '1 text',
+            '2 text',
+            '3 text'
+        ],options: {}}
+]
